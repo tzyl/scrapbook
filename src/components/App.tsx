0e2675f53961508d97dfcbd47e821ea306cbe9fd
tzyl <tim95@hotmail.co.uk>
2018-01-02 23:14:37 +0000

    Add empty Timeline

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,17 @@
 import * as React from "react";
 
 import { Header } from "./Header";
+import { Timeline } from "./Timeline";
 
 export const App: React.SFC<{}> = (props) => {
   return (
     <div>
       <Header title="Timeline" />
+      <Timeline
+        values={["2018-01-02"]}
+        indexClick={() => null}
+        index={0}
+      />
     </div>
   );
 };

fc7de2ab555c7deffefcae21a7a0d8a89c88834d
tzyl <tim95@hotmail.co.uk>
2018-01-06 20:39:31 +0000

    Implement TypeScript declaration file for react-event-timeline

--- src/custom/react-event-timeline/index.d.ts
+++ src/custom/react-event-timeline/index.d.ts
@@ -1,4 +1,35 @@
 declare module "react-event-timeline" {
-  export var Timeline: any;
-  export var TimelineEvent: any;
+  import * as React from "react";
+
+  export interface ITimelineProps {
+    children: React.ReactNode;
+  }
+  export interface ITimelineEventProps {
+    title: React.ReactNode;
+    subtitle?: React.ReactNode;
+    createdAt?: React.ReactNode;
+    children?: React.ReactNode;
+    buttons?: React.ReactNode;
+    container?: string;
+    icon?: React.ReactNode;
+    iconColor?: string;
+    iconStyle?: object;
+    bubbleStyle?: object;
+    contentStyle?: object;
+    cardHeaderStyle?: object;
+    style?: object;
+    titleStyle?: object;
+    subtitleStyle?: object;
+  }
+  export interface ITimelineBlipProps {
+    title: React.ReactNode;
+    icon?: React.ReactNode;
+    iconColor?: string;
+    iconStyle?: object;
+    style?: object;
+  }
+
+  export class Timeline extends React.Component<ITimelineProps> {}
+  export class TimelineEvent extends React.Component<ITimelineEventProps> {}
+  export class TimelineBlip extends React.Component<ITimelineEventProps> {}
 }

b8b221147a3f029c8711d7cb1554231fe1bee3b8
tzyl <tim95@hotmail.co.uk>
2018-01-06 20:57:58 +0000

    Implement TypeScript declaration file for react-photo-gallery

---
+++ src/custom/react-photo-gallery/index.d.ts
@@ -0,0 +1,30 @@
+declare module "react-photo-gallery" {
+  import * as React from "react";
+
+  export interface IGalleryProps {
+    photos: IPhotoProps[];
+    columns?: number;
+    onClick?: (event: any, photoObject: IPhotoObject) => any;
+    margin?: number;
+    ImageComponent?: JSX.Element;
+  }
+
+  export interface IPhotoProps {
+    src: string;
+    srcSet?: string[];
+    sizes?: string[];
+    width: number;
+    height: number;
+    alt?: string;
+  }
+
+  export interface IPhotoObject {
+    index: number;
+    photo: IPhotoProps;
+    previous: IPhotoProps;
+    next: IPhotoProps;
+  }
+
+  export class Gallery extends React.Component<IGalleryProps> {}
+  export class Photo extends React.PureComponent<IPhotoProps> {}
+}

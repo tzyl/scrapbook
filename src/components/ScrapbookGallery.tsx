57236c08ef64769fd34ba1a0fcf8e01d9aebe9d2
tzyl <tim95@hotmail.co.uk>
2018-01-07 14:06:42 +0000

    Create ScrapbookGallery component

---
+++ src/components/ScrapbookGallery.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+
+import Lightbox, { ILightboxProps } from "react-images";
+import Gallery, { IGalleryProps, IPhotoObject } from "react-photo-gallery";
+import { IScrapbookPhoto } from "../types/events";
+
+export interface IScrapbookGalleryProps {
+  photos: IScrapbookPhoto[];
+  galleryIsOpen: boolean;
+  lightboxIsOpen: boolean;
+  openLightbox: (event: any, photoObject: IPhotoObject) => any;
+  closeLightbox: (event: any) => any;
+  gotoPrevious: (event: any) => any;
+  gotoNext: (event: any) => any;
+  currentImage: number;
+}
+
+const ScrapbookGallery: React.SFC<IScrapbookGalleryProps> = ({
+  photos,
+  galleryIsOpen,
+  lightboxIsOpen,
+  openLightbox,
+  closeLightbox,
+  gotoPrevious,
+  gotoNext,
+  currentImage,
+}) => {
+  if (!galleryIsOpen) { return null; }
+  return (
+    <div>
+        <Gallery photos={photos} onClick={openLightbox} />
+        <Lightbox
+          images={photos}
+          onClose={closeLightbox}
+          onClickPrev={gotoPrevious}
+          onClickNext={gotoNext}
+          currentImage={currentImage}
+        />
+    </div>
+  );
+};
+
+export default ScrapbookGallery;

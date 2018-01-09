9633f926be9c52be7760f95590260fadb8c90316
tzyl <tim95@hotmail.co.uk>
2018-01-09 19:18:57 +0000

    Mock ScrapbookGallery in App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 
+import { IScrapbookPhoto } from "../types/events";
 import Header from "./Header";
+import ScrapbookGallery from "./ScrapbookGallery";
 import ScrapbookTimeline, { ITimelineEvent } from "./ScrapbookTimeline";
 
 const mockEvents: ITimelineEvent[] = [
@@ -37,11 +39,33 @@ const mockEvents: ITimelineEvent[] = [
   },
 ];
 
+const mockPhotos: IScrapbookPhoto[] = [
+  { src: require("../../public/album1/1.jpg"), width: 4, height: 3 },
+  { src: require("../../public/album1/2.jpg"), width: 1, height: 1 },
+  { src: require("../../public/album1/3.jpg"), width: 3, height: 4 },
+  { src: require("../../public/album1/4.jpg"), width: 3, height: 4 },
+  { src: require("../../public/album1/5.jpg"), width: 3, height: 4 },
+  { src: require("../../public/album1/6.jpg"), width: 4, height: 3 },
+  { src: require("../../public/album1/7.jpg"), width: 3, height: 4 },
+  { src: require("../../public/album1/8.jpg"), width: 4, height: 3 },
+  { src: require("../../public/album1/9.jpg"), width: 4, height: 3 },
+];
+
 const App: React.SFC<{}> = (props) => {
   return (
     <div>
       <Header title="Timeline" />
       <ScrapbookTimeline events={mockEvents} />
+      <ScrapbookGallery
+        photos={mockPhotos}
+        galleryIsOpen={true}
+        lightboxIsOpen={true}
+        openLightbox={null}
+        closeLightbox={null}
+        gotoPrevious={null}
+        gotoNext={null}
+        currentImage={0}
+      />
     </div>
   );
 };

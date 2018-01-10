842b5533a39857a45bb8ac01a8f83c931e734135
tzyl <tim95@hotmail.co.uk>
2018-01-10 21:59:41 +0000

    Update gallery reducer with actions
    Implement reducing state for TOGGLE_GALLERY_OPEN, TOGGLE_LIGHTBOX_OPEN,
    SELECT_PHOTO.

--- src/reducers/gallery.ts
+++ src/reducers/gallery.ts
@@ -1,4 +1,4 @@
-import { IStoreGalleryState } from "../types/gallery";
+import { GalleryActionDefinitions, IStoreGalleryState } from "../types/gallery";
 import { IAction } from "../types/redux";
 
 const defaultState: IStoreGalleryState = {
@@ -9,6 +9,21 @@ const defaultState: IStoreGalleryState = {
 
 const gallery = (state = defaultState, action: IAction): IStoreGalleryState => {
   switch (action.type) {
+    case GalleryActionDefinitions.TOGGLE_GALLERY_OPEN:
+    return {
+      ...state,
+      galleryIsOpen: !state.galleryIsOpen,
+    };
+    case GalleryActionDefinitions.TOGGLE_LIGHTBOX_OPEN:
+    return {
+      ...state,
+      lightboxIsOpen: !state.lightboxIsOpen,
+    };
+    case GalleryActionDefinitions.SELECT_PHOTO:
+      return {
+        ...state,
+        selectedPhotoIndex: action.payload.index,
+      };
     default:
       return state;
   }

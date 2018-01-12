a3ec2921ca1c51ae8da2c8089574e5632e56a40b
tzyl <tim95@hotmail.co.uk>
2018-01-12 23:26:31 +0000

    Pass store to App

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,15 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
 import App from "./components/App";
+import configureStore from "./store/configureStore";
+
+const store = configureStore();
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById("root"),
 );

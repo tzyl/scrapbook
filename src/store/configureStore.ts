fbaca6f61fa6b5d36d0e543cb13367f1725b351f
tzyl <tim95@hotmail.co.uk>
2018-01-13 23:04:07 +0000

    Configure actionCreators in Redux devtools

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -2,16 +2,25 @@ import * as localForage from "localforage";
 import { applyMiddleware, compose, createStore } from "redux";
 import logger from "redux-logger";
 import { persistReducer, persistStore } from "redux-persist";
+import { PersistConfig } from "redux-persist/es/types";
 
 import rootReducer from "../reducers";
 
-const persistConfig = {
+import * as EventsActions from "../actions/events";
+import * as GalleryActions from "../actions/gallery";
+import * as TimelineActions from "../actions/timeline";
+
+const persistConfig: PersistConfig = {
   key: "root",
   storage: localForage,
  };
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers =
+  (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      actionCreators: {...EventsActions, ...GalleryActions, ...TimelineActions},
+    }) : compose;
 
 const configureStore = () => {
   const store = createStore(

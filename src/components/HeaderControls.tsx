f2c5d8f791dcd31e6c2897be7811b3e10c6d2294
tzyl <tim95@hotmail.co.uk>
2018-02-04 16:41:30 +0000

    Add loading spinner when generating thumbnails

--- src/components/HeaderControls.tsx
+++ src/components/HeaderControls.tsx
@@ -1,25 +1,35 @@
 import * as React from "react";
 
 import { Button } from "@blueprintjs/core";
+
 import { EditorMode } from "../types/editor";
+import LoadingSpinner from "./LoadingSpinner";
+
+export interface IHeaderControlsStateProps {
+  isGeneratingThumbnails: boolean;
+}
 
 export interface IHeaderControlsDispatchProps {
   openEditor: () => any;
   setEditorMode: (mode: EditorMode) => any;
 }
 
-export type IHeaderControlsProps = IHeaderControlsDispatchProps;
+export type IHeaderControlsProps = IHeaderControlsStateProps & IHeaderControlsDispatchProps;
 
 const HeaderControls: React.SFC<IHeaderControlsProps> = ({
   openEditor,
   setEditorMode,
+  isGeneratingThumbnails,
 }) => {
   const openEditorAdd = () => {
     setEditorMode(EditorMode.add);
     openEditor();
   };
   return (
-    <Button className="pt-minimal" iconName="pt-icon-add" onClick={openEditorAdd}>Create new event</Button>
+    <div>
+      <Button className="pt-minimal" iconName="pt-icon-add" onClick={openEditorAdd}>Create new event</Button>
+      <LoadingSpinner isLoading={isGeneratingThumbnails} text="Generating thumbnails" />
+    </div>
   );
 };
 

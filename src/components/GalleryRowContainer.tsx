bd30700b59209c4d22b864a44ad12080aeb828f3
tzyl <tim95@hotmail.co.uk>
2018-02-14 22:54:17 +0000

    Work on batching photos into rows

--- src/components/GalleryRowContainer.tsx
+++ src/components/GalleryRowContainer.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 
 import { IScrapbookPhoto } from "../types/events";
+import GalleryRow from "./GalleryRow";
 
 export interface IOwnProps {
   photos: IScrapbookPhoto[];
@@ -12,7 +13,33 @@ export type IGalleryRowContainerProps = IOwnProps;
 export default class GalleryRowContainer extends React.Component<IGalleryRowContainerProps> {
   public render() {
     return (
-      <span>Placeholder</span>
+      <div className="gallery-row-container">
+        {this.renderGalleryRows()}
+      </div>
     );
+  }
+
+  // TODO: Pass indexes rather than copies of photos?
+  private renderGalleryRows() {
+    const { photos, width } = this.props;
+    const maxWidth = width - 50;
+    const maxHeight = 150;
+    let currentWidth = 0;
+    const galleryRows: JSX.Element[] = [];
+    let currentRow: IScrapbookPhoto[] = [];
+    for (const photo of photos) {
+      const scaledWidth = photo.width * maxHeight / photo.height;
+      if (currentRow && currentWidth + scaledWidth > maxWidth) {
+        galleryRows.push(<GalleryRow key={galleryRows.length} photos={[...currentRow]} height={maxHeight} />);
+        currentWidth = 0;
+        currentRow = [];
+      }
+      currentRow.push(photo);
+      currentWidth += scaledWidth;
+    }
+    if (currentRow) {
+      galleryRows.push(<GalleryRow photos={currentRow} height={maxHeight} />);
+    }
+    return galleryRows;
   }
 }

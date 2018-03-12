944f34b0fadee955631a831fabb338c9eee7f536
tzyl <tim95@hotmail.co.uk>
2018-03-12 00:30:51 +0000

    Thumbnail exif orientation

---
+++ src/util/exif.ts
@@ -0,0 +1,38 @@
+import { PhotoOrientation } from "../types/gallery";
+
+export const calculateOrientationStyle = (orientation: PhotoOrientation) => {
+  switch (orientation) {
+    case PhotoOrientation.TOP_LEFT:
+      return {};
+    case PhotoOrientation.TOP_LEFT:
+      return {
+        transform: "rotate(0deg)",
+      };
+    case PhotoOrientation.TOP_RIGHT:
+      return {
+      };
+    case PhotoOrientation.BOTTOM_RIGHT:
+      return {
+        transform: "rotate(180deg)",
+      };
+    case PhotoOrientation.BOTTOM_LEFT:
+      return {
+      };
+    case PhotoOrientation.LEFT_TOP:
+      return {
+
+      };
+    case PhotoOrientation.RIGHT_TOP:
+      return {
+        transform: "rotate(90deg)",
+      };
+    case PhotoOrientation.RIGHT_BOTTOM:
+      return {
+
+      };
+    case PhotoOrientation.LEFT_BOTTOM:
+      return {
+        transform: "rotate(270deg)",
+      };
+  }
+};
